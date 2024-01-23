b3acb213353ab481a82276127ced150b19987501
Alice_Not_Waifu <obisidiangolden@gmail.com>
2024-01-23 12:14:45 -0300

    re-typed some pokemon and a move

--- data/moves/moves.asm
+++ data/moves/moves.asm
@@ -228,7 +228,7 @@ Moves:
 	move MEAN_LOOK,    EFFECT_MEAN_LOOK,           0, NORMAL,        STATUS,   100,  5,   0
 	move ATTRACT,      EFFECT_ATTRACT,             0, NORMAL,        STATUS,   100, 15,   0
 	move SLEEP_TALK,   EFFECT_SLEEP_TALK,          0, SOUND,         STATUS,   100, 10,   0
-	move HEAL_BELL,    EFFECT_HEAL_BELL,           0, NORMAL,        STATUS,   100,  5,   0
+	move HEAL_BELL,    EFFECT_HEAL_BELL,           0, SOUND,         STATUS,   100,  5,   0
 	move RETURN,       EFFECT_RETURN,              1, NORMAL,        PHYSICAL, 100, 20,   0
 	move PRESENT,      EFFECT_PRESENT,             1, NORMAL,        PHYSICAL, 100, 15,   0
 	move FRUSTRATION,  EFFECT_FRUSTRATION,         1, NORMAL,        PHYSICAL, 100, 20,   0
